deb3091baed72a23a64450222898432f37eceedd
Jim Bo <jim@rath.us>
2018-06-02 15:57:31 -0500

    moving API database initialization into initDb.js, and cleaning up

---
+++ api/initDb.js
@@ -0,0 +1,46 @@
+// Create a database promise for a connection
+
+// NB: for higher performance, consider switching to libpq.  Changes required:
+// * below: const { Pool } = require('pg').native;
+// * Dockerfile: apt-get install -y build-essential libpq-dev
+// * package.json: "pg-native": "^3.0.0",
+
+const fs = require('fs');
+const { Pool } = require('pg');
+
+const dbUrl = process.env.DB_URL;
+const awsRdsSsl = {
+  ca: fs.readFileSync('rds-combined-ca-bundle.pem', 'utf8'),
+  rejectUnauthorized: true,
+};
+const poolConfig = {
+    connectionString: dbUrl,
+    ssl: (process.env.NODE_ENV === 'development') ? false : awsRdsSsl,
+    statement_timeout: 250, // 250ms timeout on queries
+    connectionTimeoutMillis: 250, // 250ms timeout connecting a new client
+    idleTimeoutMillis: 10000, // 10s idle time on pool connections
+    max: 3, // keep 3 connections in the pool
+};
+
+module.exports = function(logger) {
+
+    return new Promise((resolve, reject) => {
+        const pgPool = new Pool(poolConfig);
+        pgPool.connect()
+            .then((pgClient) => {
+                // install a listener to log uncaught error events
+                pgPool.on('error', (err) => {
+                    logger.error('api-db: something bad has happened! ' + JSON.stringify(err.stack, replacer, 4));
+                });
+                // release the client, log the connection, et voila
+                pgClient.release();
+                logger.info(`api-db: connected to ${dbUrl}`);
+                resolve(pgPool);
+            })
+            .catch((err) => {
+                logger.error('api-db: connection error ' + JSON.stringify(err.stack, replacer, 4));
+                reject(err);
+            });
+    });
+
+};
--- api/main.js
+++ api/main.js
@@ -22,60 +22,22 @@ const logger = winston.createLogger({
 // Create a listener promise for the API web server
 const os = require('os');
 const port = process.env.API_PORT || 3000;
-const listen = () => {
-    return new Promise((resolve, reject) => {
+const listen = new Promise(
+    (resolve, reject) => {
         app.listen(port, () => {
             logger.info(`api-server: listening on ${os.hostname()}:${port}`);
             resolve();
         });
-    });
-};
+    }
+);
 
 // Create a database promise for a connection
-// NB: for higher performance, consider switching to libpq.  Changes required:
-// * below: const { Pool } = require('pg').native;
-// * Dockerfile: apt-get install -y build-essential libpq-dev
-// * package.json: "pg-native": "^3.0.0",
-const fs = require('fs');
-const { Pool } = require('pg');
-const dbUrl = process.env.DB_URL;
-const awsRdsSsl = {
-  ca: fs.readFileSync('rds-combined-ca-bundle.pem', 'utf8'),
-  rejectUnauthorized: true,
-};
-const poolConfig = {
-    connectionString: dbUrl,
-    ssl: (process.env.NODE_ENV === 'development') ? false : awsRdsSsl,
-    statement_timeout: 250, // 250ms timeout on queries
-    connectionTimeoutMillis: 250, // 250ms timeout connecting a new client
-    idleTimeoutMillis: 10000, // 10s idle time on pool connections
-    max: 3, // keep 3 connections in the pool
-};
-const initDb = () => {
-    return new Promise((resolve, reject) => {
-        const pgPool = new Pool(poolConfig);
-        pgPool.connect()
-            .then((pgClient) => {
-                // install a listener to log uncaught error events
-                pgPool.on('error', (err) => {
-                    logger.error('api-db: something bad has happened! ' + JSON.stringify(err.stack, replacer, 4));
-                });
-                // release the client, log the connection, et voila
-                pgClient.release();
-                logger.info(`api-db: connected to ${dbUrl}`);
-                resolve(pgPool);
-            })
-            .catch((err) => {
-                logger.error('api-db: connection error ' + JSON.stringify(err.stack, replacer, 4));
-                reject(err);
-            });
-    });
-};
+const initDb = require('./initDb.js')(logger);
 
 // Start the listener and the database connection
 Promise.all([
-    listen(),
-    initDb()
+    listen,
+    initDb
 ]).then(([_, db]) => {
 
     // Install all our middleware
