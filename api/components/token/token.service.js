4fca20fd612991556043d8294fedf4eba2d2452e
Jim Bo <jim@rath.us>
2018-06-04 17:18:29 -0500

    adding command line tool to inspect users; fixing stored tokens to be encrypted

--- api/components/token/token.service.js
+++ api/components/token/token.service.js
@@ -1,6 +1,19 @@
 const crypto = require('crypto');
+const TOKEN_KEY = process.env.TOKEN_KEY;
 
 module.exports = function(db) {
+
+    const userSelectQuery = 'select uid, fullName, email, issued, token from users where email=$1 and revoked is null';
+    const inspectUser = async (email) => {
+        const userSelectResult = await db.query(userSelectQuery, [email]);
+        if (!userSelectResult.rows.length) { throw 'No such user.'; }
+        const user = userSelectResult.rows[0];
+        const tokenDecipher = crypto.createDecipher('aes256', TOKEN_KEY);
+        let tokenString = tokenDecipher.update(user.token, 'base64').toString('binary');
+        tokenString += tokenDecipher.final().toString('binary');
+        user.token = tokenString;
+        return user;
+    };
 
     const userCountQuery = 'select count(*) from users where email=$1 and revoked is null';
     const userInsertQuery = 'insert into users (fullName, email, issued, revoked, token) values ($1, $2, $3, null, $4) returning uid';
@@ -11,15 +24,24 @@ module.exports = function(db) {
         if (userCount) { throw 'User is already registered.'; }
         // pick a token, and create a new user entry
         const tokenBuffer = crypto.randomBytes(10);
-        const userInsertValues = [fullName, email, Date.now(), tokenBuffer.toString('base64')];
+        const tokenCipher = crypto.createCipher('aes256', TOKEN_KEY);
+        let tokenCrypt = tokenCipher.update(tokenBuffer, 'buffer', 'base64');
+        tokenCrypt += tokenCipher.final('base64');
+        const userInsertValues = [fullName, email, Date.now(), tokenCrypt];
         const userInsertResult = await db.query(userInsertQuery, userInsertValues);
-        const uid = userInsertResult.rows[0].uid;
         // return the UID of the inserted row and the generated token
-        return { uid: uid, token: tokenBuffer.toString('binary') };
+        const uid = userInsertResult.rows[0].uid;
+        const tokenString = tokenBuffer.toString('binary');
+        return { uid: uid, token: tokenString };
+    };
+
+    const revokeUser = async (email) => {
     };
 
     return {
+        inspectUser: inspectUser,
         registerUser: registerUser,
+        revokeUser: revokeUser,
     };
 
 };
--- api/inspectUser.js
+++ api/inspectUser.js
@@ -1,4 +1,4 @@
-// Register a new user from the command line.
+// Inspect an existing user from the command line.
 
 const readline = require('readline');
 const readlineStd = readline.createInterface({
@@ -21,15 +21,16 @@ const askQuestions = (questions, finish, answers) => {
 
 const initDb = require('./initDb.js')(console);
 initDb.then((db) => {
-    const { registerUser } = require('./components/token/token.service.js')(db);
-    askQuestions(['Full Name', 'Email'], async (answers) => {
+    const { inspectUser } = require('./components/token/token.service.js')(db);
+    askQuestions(['Email'], async (answers) => {
         try {
-            const { uid, token } = await registerUser(...answers);
+            const user = await inspectUser(...answers);
             const stringToDictionary = require('./tokenDictionary.js');
-            console.log(`UID: ${uid}`);
-            console.log(`token: ${stringToDictionary(token)}`);
-            const logger = require('./initLogger.js');
-            logger.info(`registered user ${uid} ${answers}`);
+            console.log(`UID: ${user.uid}`);
+            console.log(`Full name: ${user.fullname}`); // NB: capitalization b/c of postgres column name insensitivity
+            console.log(`Email: ${user.email}`);
+            console.log(`Issued: ${new Date(Number(user.issued))}`);
+            console.log(`token: ${stringToDictionary(user.token)}`);
         } catch (err) {
             console.error(err);
             return;
