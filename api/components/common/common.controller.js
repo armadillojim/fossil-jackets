9b4388ff88ae1e9b0a9e2d74bd0d5d1a5ecff3bd
Jim Bo <jim@rath.us>
2018-06-06 09:38:50 -0500

    moving signature payload order standardization to service

--- api/components/common/common.controller.js
+++ api/components/common/common.controller.js
@@ -1,17 +1,3 @@
-// Sort the keys on the array objects to get a stable JSON.  We rely on ES6
-// behavior: keys are iterated over in the order they were added.  Also consider
-// libraries such as:
-// * https://www.npmjs.com/package/json-stable-stringify
-// * https://www.npmjs.com/package/jsonlint
-// * https://stackoverflow.com/a/31102605
-function sortObject(unordered) {
-    const ordered = {};
-    Object.keys(unordered).sort().forEach(function(key) {
-        ordered[key] = unordered[key];
-    });
-    return ordered;
-}
-
 module.exports = function(commonService) {
 
     const verifySignature = async (path, query, body) => {
@@ -22,7 +8,7 @@ module.exports = function(commonService) {
         const timeSynchronized = Math.abs(Date.now() - query.time) < 60000;
         if (!timeSynchronized) { throw { status:400, message:'Bad request: check your clock' }; }
         // aggregate in regular order all the variable data used to sign
-        const payload = sortObject({ path:path, ...query, ...body });
+        const payload = { path:path, ...query, ...body };
         delete payload.hmac;
         // ask the service to verify the signature using the user's key
         const validHmac = await commonService.verifySignature(payload, query.hmac, query.uid);
--- api/components/common/common.service.js
+++ api/components/common/common.service.js
@@ -1,3 +1,17 @@
+// Sort the keys on the array objects to get a stable JSON.  We rely on ES6
+// behavior: keys are iterated over in the order they were added.  Also consider
+// libraries such as:
+// * https://www.npmjs.com/package/json-stable-stringify
+// * https://www.npmjs.com/package/jsonlint
+// * https://stackoverflow.com/a/31102605
+function sortObject(unordered) {
+    const ordered = {};
+    Object.keys(unordered).sort().forEach(function(key) {
+        ordered[key] = unordered[key];
+    });
+    return ordered;
+}
+
 const crypto = require('crypto');
 const TOKEN_KEY = process.env.TOKEN_KEY;
 
@@ -5,6 +19,8 @@ module.exports = function(db) {
 
     const keyFromUidQuery = 'select token from users where uid=$1 and revoked is null';
     const verifySignature = async (payload, hmacThem, uid) => {
+        // sort the payload into a standard order, and JSONify it
+        const payloadJSON = JSON.stringify(sortObject(payload));
         // get the user's token from the database
         const keyFromUidResult = await db.query(keyFromUidQuery, [uid]);
         if (!keyFromUidResult.rows.length) { return false; }
@@ -15,7 +31,7 @@ module.exports = function(db) {
         token += tokenDecipher.final().toString('binary');
         // use the token as the key to compute the HMAC
         const hmac = crypto.createHmac('sha256', token);
-        hmac.update(JSON.stringify(payload), 'utf8');
+        hmac.update(payloadJSON, 'utf8');
         hmacUs = hmac.digest('base64');
         // check we match their signature
         return (hmacUs === hmacThem);
