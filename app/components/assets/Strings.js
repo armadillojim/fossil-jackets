393364cbce4aabaa615acff484193aa421e3db20
Jim Bo <jim@rath.us>
2018-06-09 18:45:14 -0500

    alphabetizing app Strings.js, and adding comment noting component

--- app/components/assets/Strings.js
+++ app/components/assets/Strings.js
@@ -3,18 +3,20 @@ const locale = I18nManager.localeIdentifier;
 
 Strings = {
   en_US: {
+    // HomeScreen
     appName: 'Fossil Jackets',
     newJacket: 'New Jacket',
+    signOut: 'Sign Out',
     uploadJackets: 'Upload Jackets',
     viewJacket: 'View Jacket',
-    signOut: 'Sign Out',
   },
   es_MX: {
+    // HomeScreen
     appName: 'Sobrecubiertas Fósil',
     newJacket: 'Sobrecubierta Nueva',
+    signOut: 'Cerrar Sesión',
     uploadJackets: 'Subir Sobrecubiertas',
     viewJacket: 'Ver Sobrecubierta',
-    signOut: 'Cerrar Sesión',
   },
 };
 
