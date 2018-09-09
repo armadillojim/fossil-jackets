10e6b82f7ebc0b828d8d582c9d6e55554f7cc072
Jim Bo <jim@rath.us>
2018-09-09 18:02:40 -0500

    stubbing out routing and pages for client

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Login from '../login/Login.js';
+import Table from '../table/Table.js';
+import Item from '../item/Item.js';
+
+const loggedInRender = () => {
+  return sessionStorage.getItem('credentials') ? (
+    <Redirect to="/jacket" />
+  ) : (
+    <Redirect to="/login" />
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="jumbotron text-center">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to fossil jackets!</h1>
+      <Router>
+        <div className="App">
+          <div className="jumbotron text-center">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="container">
+            <Route exact path="/" render={loggedInRender}/>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/jacket" component={Table} />
+            <Route exact path="/jacket/:jid" component={Item} />
+          </div>
         </div>
-        <div className="container">
-        </div>
-      </div>
+      </Router>
     );
   }
 }
